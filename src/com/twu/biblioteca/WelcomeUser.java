14cfcbe431dc0a6fe7029359350a5219df9da0a3
Steven Mitchell <smitchell@nasmitchel.local>
2016-01-12 14:41:37 -0600

    Completed first user story Welcome User

---
+++ src/com/twu/biblioteca/WelcomeUser.java
@@ -0,0 +1,15 @@
+package com.twu.biblioteca;
+
+import java.io.PrintStream;
+
+public class WelcomeUser {
+
+    private PrintStream printStream;
+
+    public WelcomeUser(PrintStream printStream){
+        this.printStream = printStream;
+    }
+    public void welcomeUser() {
+        printStream.println("Welcome, user!");
+    }
+}
